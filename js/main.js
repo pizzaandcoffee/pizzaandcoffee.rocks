334e0df6de4f3116690312b51d6b9be58e51b0f1
Runebaas <runebaas@gmail.com>
2016-09-04 21:39:25 +0200

    fixes

--- js/main.js
+++ js/main.js
@@ -30,3 +30,17 @@ $( document ).ready(function() {
         }
     })
 });
+
+//------------------------------------------------------------------------------
+// Scroll thingy
+//------------------------------------------------------------------------------
+$(document).on('click', 'a[href^="#"]', function(e) {\
+	var id = $(this).attr('href');
+	var $id = $(id);
+	if ($id.length === 0) {
+		return;
+	}
+	e.preventDefault();
+	var pos = $(id).offset().top;
+	$('body, html').animate({scrollTop: pos});
+});
