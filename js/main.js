be8de653c9f48010ecf090253051a196fab87b13
Runebaas <runebaas@gmail.com>
2016-09-06 16:16:46 +0200

    added dynamic generated modals

--- js/main.js
+++ js/main.js
@@ -18,17 +18,35 @@ var githubRepos = (function($, w, undefined) {
 //------------------------------------------------------------------------------
 // Script
 //------------------------------------------------------------------------------
+
+//$( document ).ready(function() {
+//    console.log("ahrg");
+//    githubRepos.run(function(data) {
+//        console.log(data[0].id);
+//        for (var i = 0; i < data.length; i++) {
+//            if (!data[i].fork) {
+//                var old = $("#projectlist").html();
+//                $("#projectlist").html(old + "<li>" + data[i].name + "</li>");
+//            }
+//        }
+//    })
+//});
+
 $( document ).ready(function() {
-    console.log("ahrg");
-    githubRepos.run(function(data) {
-        console.log(data[0].id);
-        for (var i = 0; i < data.length; i++) {
-            if (!data[i].fork) {
-                var old = $("#projectlist").html();
-                $("#projectlist").html(old + "<li>" + data[i].name + "</li>");
-            }
-        }
-    })
+	githubRepos.run(function(data) {
+		var modalhtml = "";
+		var j = 0;
+		for (var i = 0; i < data.length; i++) {
+			if (!data[i].fork) {
+				modalhtml = modalhtml + '<li><a id="demo' + i + '" href="#repo' + i + 'Modal">' + data[i].name + '</a><div id="repo' + i + 'Modal"><div class="close-repo' + i + 'Modal">CLOSE MODAL</div><div class="modal-content">' + data[i].git_url + '</div></div></li>';
+			}
+			j = i;
+		}
+		$("#projectlist").html(modalhtml);
+		for (var i = 0; i <= j; i++) {                    
+			$("#demo" + i).animatedModal({modalTarget: "repo" + i + "Modal"});
+		}
+	})
 });
 
 //------------------------------------------------------------------------------
