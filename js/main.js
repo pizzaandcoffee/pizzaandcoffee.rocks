987cc6e59cd39044587bcd5d98e4655c8d8d604a
Alex Fence <alexfence.code@gmail.com>
2016-09-06 19:08:11 +0200

    fixed Modals

--- js/main.js
+++ js/main.js
@@ -16,49 +16,51 @@ var githubRepos = (function($, w, undefined) {
 
 
 //------------------------------------------------------------------------------
+// Functions
+//------------------------------------------------------------------------------
+//adds a ScrollyThingy to the links of the navbar
+function setScrollyThingy() {
+    $(document).on('click', 'a[href^="#"]', function(e) {
+    	var id = $(this).attr('href');
+    	var $id = $(id);
+    	if ($id.length === 0) {
+    		return;
+    	}
+    	e.preventDefault();
+    	var pos = $(id).offset().top;
+    	$('body, html').animate({scrollTop: pos});
+    });
+}
+
+//------------------------------------------------------------------------------
 // Script
 //------------------------------------------------------------------------------
-
-//$( document ).ready(function() {
-//    console.log("ahrg");
-//    githubRepos.run(function(data) {
-//        console.log(data[0].id);
-//        for (var i = 0; i < data.length; i++) {
-//            if (!data[i].fork) {
-//                var old = $("#projectlist").html();
-//                $("#projectlist").html(old + "<li>" + data[i].name + "</li>");
-//            }
-//        }
-//    })
-//});
-
 $( document ).ready(function() {
+    setScrollyThingy();
+
+    //generate the Project list and add Modals
 	githubRepos.run(function(data) {
 		var modalhtml = "";
-		var j = 0;
+		var projectCount = 0;
 		for (var i = 0; i < data.length; i++) {
 			if (!data[i].fork) {
-				modalhtml = modalhtml + '<li><a id="demo' + i + '" href="#repo' + i + 'Modal">' + data[i].name + '</a><div id="repo' + i + 'Modal"><div class="close-repo' + i + 'Modal">CLOSE MODAL</div><div class="modal-content">' + data[i].git_url + '</div></div></li>';
+                //god awful string of doom
+				modalhtml = modalhtml + '<li><a id="demo' + i + '" href="#repo' + i + 'Modal">' + data[i].name + '</a><div id="repo' + i + 'Modal"><div class="close-repo' + i + 'Modal modalclose"><img src="img/close.png"/></div><div class="modal-content">' + data[i].git_url + '</div></div></li>';
 			}
-			j = i;
+			projectCount = i;
 		}
 		$("#projectlist").html(modalhtml);
-		for (var i = 0; i <= j; i++) {                    
-			$("#demo" + i).animatedModal({modalTarget: "repo" + i + "Modal"});
+        //add modalsa
+		for (var i = 0; i <= projectCount; i++) {
+			$("#demo" + i).animatedModal({
+                modalTarget:'repo' + i + "Modal",
+                animatedIn:'fadeInLeft',
+                animatedOut:'fadeOutRight',
+                color:'#eeeeee',
+                afterClose: function() {
+                    setScrollyThingy();
+                }
+            });
 		}
 	})
-});
-
-//------------------------------------------------------------------------------
-// Scroll thingy
-//------------------------------------------------------------------------------
-$(document).on('click', 'a[href^="#"]', function(e) {
-	var id = $(this).attr('href');
-	var $id = $(id);
-	if ($id.length === 0) {
-		return;
-	}
-	e.preventDefault();
-	var pos = $(id).offset().top;
-	$('body, html').animate({scrollTop: pos});
 });
