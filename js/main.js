32e8bfe924e79595a7a4989796e536fa1c556cec
Alex Fence <alexfence.code@gmail.com>
2016-09-06 21:24:19 +0200

    added markdownHandler

--- js/main.js
+++ js/main.js
@@ -6,10 +6,51 @@ var githubRepos = (function($, w, undefined) {
     var url = "https://api.github.com/users/pizzaandcoffee/repos";
 
     return {
+        //hand a function over that does something with our githubRepos
         run: function(handler) {
             $.getJSON(url, function(data){
                 handler(data);
             });
+        }
+    };
+}(jQuery, window));
+
+var markdownHandler = (function($, w, undefined) {
+
+    var folder = "md/";
+
+    var md = new w.markdownit();
+
+    var _fileExists = function(name) {
+        w.console.log(folder + name + ".md");
+        $.ajax(folder + name + ".md", {
+
+           error: function (response) {
+               w.console.log("404");
+               w.console.log(name);
+               _get404(name);
+           },
+           success: function (data) {
+               w.console.log("200");
+               _addTextToModal(name, data);
+           }
+        });
+    };
+
+    var _addTextToModal = function(name, markdown) {
+         w.console.log(name);
+         var html = md.render(markdown);
+         $("#modal-" + name + "-content").html(html);
+    };
+
+    var _get404 = function(name) {
+        _addTextToModal(name, "# Sorry \n ## FUCK!");
+    };
+
+    return {
+        getModalContent: function(name) {
+            w.console.log("fuck");
+            _fileExists(name);
         }
     };
 }(jQuery, window));
@@ -45,7 +86,7 @@ $( document ).ready(function() {
 		for (var i = 0; i < data.length; i++) {
 			if (!data[i].fork) {
                 //god awful string of doom
-				modalhtml = modalhtml + '<li><a id="demo' + i + '" href="#repo' + i + 'Modal">' + data[i].name + '</a><div id="repo' + i + 'Modal"><div class="close-repo' + i + 'Modal modalclose"><img src="img/close.png"/></div><div class="modal-content">' + data[i].git_url + '</div></div></li>';
+				modalhtml = modalhtml + '<li><a id="demo' + i + '" href="#repo' + i + 'Modal">' + data[i].name + '</a><div id="repo' + i + 'Modal"><div class="close-repo' + i + 'Modal modalclose"><img src="img/close.png"/></div><div id="modal-' + data[i].name + '-content">' + data[i].git_url + '</div></div></li>';
 			}
 			projectCount = i;
 		}
@@ -57,6 +98,10 @@ $( document ).ready(function() {
                 animatedIn:'fadeInLeft',
                 animatedOut:'fadeOutRight',
                 color:'#eeeeee',
+                beforeOpen: function() {
+                    //fuck
+                    markdownHandler.getModalContent("Geekbot");
+                },
                 afterClose: function() {
                     setScrollyThingy();
                 }
---
+++ md/Geekbot.md
@@ -0,0 +1,76 @@
+# Geekbot
+
+A bot framework for discord built on [DiscordPHP](https://github.com/teamreflex/DiscordPHP) for easier bot development
+
+We are Restructuring parts of the code currently to create new features, some code or all of it may not work on certain commits.
+
+### Features and ToDo
+
+##### Features for Developers
+
+- [x] Commands as Modules
+- [x] Built-in Database
+- [x] Documentation
+- [x] Message recording and level system
+- [x] Api Wrapper for easier and more intuitive development
+- [x] Permission Wrapper for easy command permission handling
+- [x] easy to read and well documented and easy extensible config file
+- [x] automatic help command
+- [ ] async based on the promises system
+- [ ] parent and child commands
+- [ ] RESTful API
+- [ ] Installation Script
+
+##### Features for Users (includes base commands)
+
+- [x] Prefix System to avoid collision with other bots
+- [x] kick and ban users
+- [x] all commands work in direct messages
+- [x] level system, look how many images you or someone else has written
+- [x] automatic reactions to certain words
+- [x] Look up someones overwatch stats
+- [x] a pokedex
+- [x] search for youtube videos
+- [x] get images from several image boards like konachan and gelbooru
+- [x] get a random image from 4chan or from a specified board
+- [x] look up anime information from myanime list
+- [x] count how many bad jokes someone made
+- [x] get random cat images together with an unrelated cat fact
+- [x] get a fortune from a database with over 9000 fortunes
+- [x] 8ball
+- [x] throw a coin or a dice
+- [ ] lots of other commands
+- [ ] web interface for easy bot management
+
+
+### Documentation
+
+----------------
+
+The Documentation can be found in the docs folder | **Outdated \***
+
+The Code itself is also pretty well documentated and there are a lot
+of different [example modules here](https://github.com/runebaas/Geekbot-Modules)
+
+\* Even though the docs are outdated because of the current code restructuring, most of it should still give you a general idea of how everything works
+
+### Contributing
+
+----------------
+
+Everyone is welcome to contribute!
+
+Make a pull request whenever you want to and we'll review it
+
+### Credits
+
+----------------
+
+* Daan Boerlage | [Github](https://github.com/runebaas) | [LinkedIn](https://ch.linkedin.com/in/dboerlage) | [Website](https://boerlage.me)
+* Alex Fence | [Github](https://github.com/AlexFence)
+
+### License
+
+----------------
+
+Geekbot is open for everyone under the [GNU Public License v3](http://www.gnu.org/licenses/gpl-3.0.html)
