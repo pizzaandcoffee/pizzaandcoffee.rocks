5738d7acf6457419a2b4b4299de976693b4aecdc
runebaas <runebaas@gmail.com>
2016-09-10 20:26:51 +0200

    design updates

--- js/main.js
+++ js/main.js
@@ -1,4 +1,39 @@
 //------------------------------------------------------------------------------
+// Gradient background
+//------------------------------------------------------------------------------
+
+var granimInstance = new Granim({
+    element: '#canvas-interactive',
+    name: 'interactive-gradient',
+    elToSetClassOn: '.canvas-interactive-wrapper',
+    direction: 'diagonal',
+    opacity: [1, 1],
+    isPausedWhenNotInView: true,
+    stateTransitionSpeed: 500,
+    states : {
+        "default-state": {
+            gradients: [
+                ['#B3FFAB', '#12FFF7'],
+                ['#ADD100', '#7B920A'],
+                ['#1A2980', '#26D0CE']
+            ],
+            transitionSpeed: 10000
+        },
+        "violet-state": {
+            gradients: [
+                ['#9D50BB', '#6E48AA'],
+                ['#4776E6', '#8E54E9']
+            ],
+            transitionSpeed: 2000
+        },
+        "orange-state": {
+            gradients: [ ['#FF4E50', '#F9D423'] ],
+            loop: false
+        }
+    }
+});
+
+//------------------------------------------------------------------------------
 // Modules
 //------------------------------------------------------------------------------
 var githubRepos = (function($, w, undefined) {
