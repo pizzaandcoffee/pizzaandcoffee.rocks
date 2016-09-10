afc8ac3814f84d576859e6478a628a42d46c6b3a
runebaas <runebaas@gmail.com>
2016-09-10 21:50:28 +0200

    small design fixes

--- js/main.js
+++ js/main.js
@@ -18,17 +18,6 @@ var granimInstance = new Granim({
                 ['#1A2980', '#26D0CE']
             ],
             transitionSpeed: 10000
-        },
-        "violet-state": {
-            gradients: [
-                ['#9D50BB', '#6E48AA'],
-                ['#4776E6', '#8E54E9']
-            ],
-            transitionSpeed: 2000
-        },
-        "orange-state": {
-            gradients: [ ['#FF4E50', '#F9D423'] ],
-            loop: false
         }
     }
 });
