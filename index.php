aba3a7789bd11407bee1ff94783f151cf9e9da32
runebaas <runebaas@gmail.com>
2016-09-11 19:44:02 +0200

    Small Fixes

--- index.php
+++ index.php
@@ -6,14 +6,17 @@ Flight::route('/', function(){
 });
 
 Flight::route('/fuck', function(){
-  echo  "fuck";
+  echo  "<h1>And you thought nothing would be here? well guess again motherfucker</h1>";
 });
 
 
 Flight::route('/ajax/modals/@name', function($name){
     $pd = new Parsedown();
     if(file_exists("md/" . $name . ".md")){
-        echo $pd->text(file_get_contents("md/" . $name . ".md"));
+        $output = $pd->text(file_get_contents("md/" . $name . ".md"));
+        $output = str_replace('[x]', '☑️', $output);
+        $output = str_replace('[ ]', '☐️', $output);
+        echo $output;
     } else {
         echo $pd->text(file_get_contents("md/404.md"));
     }
--- js/main.js
+++ js/main.js
@@ -98,12 +98,11 @@ $( document ).ready(function() {
 			if (!data[i].fork) {
                 projectlist.push(data[i].name);
                 //god awful string of doom
-				modalhtml = modalhtml + '<li><a id="demo' + i + '" href="#repo' + i + 'Modal">' + data[i].name + '</a><div id="repo' + i + 'Modal"><div class="close-repo' + i + 'Modal modalclose"><img src="img/close.png"/></div><div id="modal-' + data[i].name + '-content">' + data[i].git_url + '</div></div></li>';
+				modalhtml = modalhtml + '<li><a id="demo' + i + '" href="#repo' + i + 'Modal">' + data[i].name + '</a><div id="repo' + i + 'Modal"><div class="close-repo' + i + 'Modal modalclose"><img style="float: right;" src="img/close.png"/></div><div id="modal-' + data[i].name + '-content">' + data[i].git_url + '</div></div></li>';
 			}
 			projectCount += 1;
 		}
 		$("#projectlist").html(modalhtml);
-        //add modalsa
 		for (var i = 0; i < projectCount; i++) {
 			$("#demo" + i).animatedModal({
                 modalTarget:'repo' + i + "Modal",
